1734aef7460f87d197ff979060228c9a5532855f
David Naumann <NaumannDaveK@gmail.com>
2019-02-23 00:52:27 -0600

    Added NAND_SC (self component) gates and test benches

--- nands.v
+++ nands.v
@@ -69,11 +69,11 @@ module NAND3_C(
 	NOT1 N2 (.i1(i2), .o(i2_n));
 	NOT1 N3 (.i1(i3), .o(i3_n));
 
-	OR3 O1 (.i1(i1_n), .i2(i2_n), i3(i3_n), .o(o));
+	OR3 O1 (.i1(i1_n), .i2(i2_n), .i3(i3_n), .o(o));
 
 endmodule
 
-module NAND3_C(
+module NAND5_C(
 	input i1, // input 1
 	input i2, // input 2
 	input i3, // input 3
@@ -87,8 +87,53 @@ module NAND3_C(
 	NOT1 N2 (.i1(i2), .o(i2_n));
 	NOT1 N3 (.i1(i3), .o(i3_n));
 	NOT1 N4 (.i1(i4), .o(i4_n));
-	NOT1 N3 (.i1(i5), .o(i5_n));
+	NOT1 N5 (.i1(i5), .o(i5_n));
 
-	OR5 O1 (.i1(i1_n), .i2(i2_n), i3(i3_n), .i4(i4_n), i5(i5_n), .o(o));
+	OR5 O1 (.i1(i1_n), .i2(i2_n), .i3(i3_n), .i4(i4_n), .i5(i5_n), .o(o));
+
+endmodule
+
+// NAND_SC Gate Modules
+
+module NAND2_SC(
+	input i1, // input 1
+	input i2, // input 2
+	output o // output
+);
+
+	NAND2_E NA1 (.i1(i1), .i2(i2), .o(o));
+
+endmodule
+
+module NAND3_SC(
+	input i1, // input 1
+	input i2, // input 2
+	input i3, // input 3
+	output o // output
+);
+	wire tempo_1, tempo_2; // temporary outputs
+	
+	NAND2_E NA1 (.i1(i1), .i2(i2), .o(tempo_1));
+	NAND2_E NA2 (.i1(tempo_1), .i2(tempo_1), .o(tempo_2));
+	NAND2_E NA3 (.i1(tempo_2), .i2(i3), .o(o));
+endmodule
+
+module NAND5_SC(
+	input i1, // input 1
+	input i2, // input 2
+	input i3, // input 3
+	input i4, // input 3
+	input i5, // input 3
+	output o // output
+);
+	wire tempo_1, tempo_2, tempo_3, tempo_4, tempo_5, tempo_6; // temporary outputs
+	
+	NAND2_E NA1 (.i1(i1), .i2(i2), .o(tempo_1));
+	NAND2_E NA2 (.i1(i3), .i2(i4), .o(tempo_2));
+	NAND2_E NA3 (.i1(tempo_1), .i2(tempo_1), .o(tempo_3));
+	NAND2_E NA4 (.i1(tempo_2), .i2(tempo_2), .o(tempo_4));
+	NAND2_E NA5 (.i1(tempo_3), .i2(tempo_4), .o(tempo_5));
+	NAND2_E NA6 (.i1(tempo_5), .i2(tempo_5), .o(tempo_6));
+	NAND2_E NA7 (.i1(tempo_6), .i2(i5), .o(o));
 
 endmodule
--- tb_nands.v
+++ tb_nands.v
@@ -223,3 +223,112 @@ module TB_NAND5_C;
 	end
 	
 endmodule
+
+// NAND_SC Testbenches
+
+module TB_NAND2_SC;
+	
+	reg i1, i2; // inputs
+	wire o; // output
+	
+	task apply_test (input test_i1, input test_i2);
+		begin
+			i1 = test_i1;
+			i2 = test_i2;
+			#100;
+		end
+	endtask
+	
+	// instantiates the NAND2 gate
+	NAND2_SC duv (.i1(i1), .i2(i2), .o(o));
+	
+	// Procedure statement
+	initial begin
+		apply_test (0, 0);
+		apply_test (0, 1);
+		apply_test (1, 0);
+		apply_test (1, 1);
+		#100;
+	end
+	
+endmodule
+
+
+module TB_NAND3_SC;
+	
+	// Constant for inputs
+	localparam CONSTANT_INPUTS = 3;
+	
+	// integer for counting
+	integer counter = 0;
+	
+	// inputs and test inputs
+	reg i1, i2, i3; // inputs
+	reg [CONSTANT_INPUTS-1:0] test_i = 3'b000; // temporary inputs
+	
+	// output
+	wire o; // output
+	
+	task apply_test (input [CONSTANT_INPUTS-1:0] test_i);
+		begin
+			i1 = test_i[0];
+			i2 = test_i[1];
+			i3 = test_i[2];
+			#100;
+		end
+	endtask
+	
+	// instantiates the NAND3 gate
+	NAND3_SC duv (.i1(i1), .i2(i2), .i3(i3), .o(o));
+	
+	// Procedure statement
+	initial begin
+		for (counter = 0; counter < (2**(CONSTANT_INPUTS)); counter = counter + 1) begin
+			apply_test(test_i);
+			test_i = test_i + 1'b1;
+		end
+		#100;
+	end
+	
+endmodule
+
+
+module TB_NAND5_SC;
+	
+	// Constant for inputs
+	localparam CONSTANT_INPUTS = 5;
+	
+	// integer for counting
+	integer counter = 0;
+	
+	// inputs and test inputs
+	reg i1, i2, i3, i4, i5; // inputs
+	reg [CONSTANT_INPUTS-1:0] test_i = 5'b00000; // temporary inputs
+	
+	// output
+	wire o; // output
+	
+	task apply_test (input [CONSTANT_INPUTS-1:0] test_i);
+		begin
+			i1 = test_i[0];
+			i2 = test_i[1];
+			i3 = test_i[2];
+			i4 = test_i[3];
+			i5 = test_i[4];
+			#100;
+		end
+	endtask
+	
+	// instantiates the NAND5 gate
+	NAND5_SC duv (.i1(i1), .i2(i2), .i3(i3), .i4(i4), .i5(i5), .o(o));
+	
+	// Procedure statement
+	initial begin
+		for (counter = 0; counter < (2**(CONSTANT_INPUTS)); counter = counter + 1) begin
+			apply_test(test_i);
+			test_i = test_i + 1'b1;
+		end
+		#100;
+	end
+	
+endmodule
